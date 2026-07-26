b465528fdb9906aa693adc72015a85c3eac64676
hrishick <hrishickrudhresh@gmail.com>
2026-07-26 21:46:50 +0530

    Frontend API update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Register from "./pages/register";
 import Todo from "./pages/Todo";
 
 function App() {
